57112970e7aec025c6e4f1cb8d8ffd6569dde945
notjack <89240346+nootjack@users.noreply.github.com>
2022-11-25 23:32:40 -0800

    Added generateLinks

---
+++ index.ts
@@ -0,0 +1,12 @@
+import { generateLinks } from './modules/generateLinks';
+
+const BASE_WEBSITES = [
+  'youtube',
+  'facebook',
+  'gmail',
+  'twitter',
+  'reddit',
+  'instagram',
+];
+
+console.log(generateLinks(BASE_WEBSITES));
---
+++ modules/generateLinks.ts
@@ -0,0 +1,34 @@
+const BASE_WEBSITES = [
+  'youtube',
+  'facebook',
+  'gmail',
+  'twitter',
+  'reddit',
+  'instagram',
+];
+
+export const generateLinks = (base: Array<String>, settings?: Object) => {
+  let output = new Array<String>();
+  for (let i = 0; i < base.length; i++) {
+    let link = base[i];
+    output = [...output, ...missingLetters(link), ...extraLetters(link)];
+  }
+  return [...new Set(output)];
+};
+
+const missingLetters = (base: String) => {
+  let links: Array<String> = [];
+  let link = base;
+  for (let i = 0; i < link.length; i++) {
+    links.push(link.slice(0, i) + link.slice(i + 1));
+  }
+  return links;
+};
+const extraLetters = (base: String) => {
+  let links: Array<String> = [];
+  let link = base;
+  for (let i = 0; i < link.length; i++) {
+    links.push(link.slice(0, i) + link[i] + link.slice(i));
+  }
+  return links;
+};
