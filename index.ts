18e43e0ffac71fda31f2c9fa863161e275914b90
notjack <89240346+nootjack@users.noreply.github.com>
2022-11-26 23:04:44 -0800

    Make express server

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { generateLinks } from './modules/generateLinks';
+const express = require('express');
 const path = require('path');
 
 const BASE_WEBSITES = [
@@ -10,4 +11,19 @@ const BASE_WEBSITES = [
   'instagram',
 ];
 
-console.log(generateLinks(BASE_WEBSITES));
+const app = express();
+const port = 8080;
+
+// TODO: Add correct types for req and res, right now the express types package is broken
+app.get('/', (req: any, res: any) => {
+  res.sendFile(path.join(__dirname, '/views/index.html'));
+});
+
+app.get('/generate-links', (req: any, res: any) => {
+  console.log(generateLinks(BASE_WEBSITES));
+  res.redirect('/');
+});
+
+app.listen(port, () => {
+  console.log(`Running at http://localhost:${port}`);
+});
