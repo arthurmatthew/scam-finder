4d7424f84bedc16386f9f403c14b3fb5c236889c
notjack <89240346+nootjack@users.noreply.github.com>
2022-11-26 23:45:45 -0800

    Remove express server (temporarily?)

--- index.ts
+++ index.ts
@@ -1,6 +1,4 @@
 import { generateLinks } from './modules/generateLinks';
-const express = require('express');
-const path = require('path');
 
 const BASE_WEBSITES = [
   'youtube',
@@ -10,20 +8,3 @@ const BASE_WEBSITES = [
   'reddit',
   'instagram',
 ];
-
-const app = express();
-const port = 8080;
-
-// TODO: Add correct types for req and res, right now the express types package is broken
-app.get('/', (req: any, res: any) => {
-  res.sendFile(path.join(__dirname, '/views/index.html'));
-});
-
-app.get('/generate-links', (req: any, res: any) => {
-  console.log(generateLinks(BASE_WEBSITES));
-  res.redirect('/');
-});
-
-app.listen(port, () => {
-  console.log(`Running at http://localhost:${port}`);
-});
